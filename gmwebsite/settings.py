afac3cf23addfbe0e493c35d7ee2bb0a4f1e9c43
swati gaikwad <swatirgaikwad1436@gmail.com>
2026-02-01 16:59:45 +0530

    Content updated

--- gmwebsite/settings.py
+++ gmwebsite/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 # Load environment variables from .env file
 # load_dotenv()
-
+    
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
